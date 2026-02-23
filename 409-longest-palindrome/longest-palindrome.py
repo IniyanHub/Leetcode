3ca5c2c1d39202aaf2c70a638819693663c9e4ed
Iniyan <iniyan.workhub@gmail.com>
2026-02-23 09:14:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (24.08%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,13 +1,17 @@
+from collections import Counter
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         freq = Counter(s)
         length = 0
         odd_exists = False
+
         for count in freq.values():
-            length += (count // 2) * 2
+            length += (count // 2) * 2   # add even pairs
             if count % 2 == 1:
                 odd_exists = True
+
         if odd_exists:
-            length += 1
+            length += 1   # one odd character can be the center
+
         return length
         
