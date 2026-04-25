cd68fa6bb83b1db5676ddae77d06aea112792bdb
Iniyan <iniyan.workhub@gmail.com>
2026-04-25 23:11:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (43.19%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,27 +1,24 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
-        current_string = ""
-        current_num = 0
-        
+        curr_str = ""
+        curr_num = 0
+
         for char in s:
             if char.isdigit():
-                # Build the multi-digit number
-                current_num = current_num * 10 + int(char)
+                curr_num = curr_num * 10 + int(char)
+
             elif char == '[':
-                # Push the current context to the stack
-                stack.append((current_string, current_num))
-                # Reset for the new inner scope
-                current_string = ""
-                current_num = 0
+                stack.append((curr_str, curr_num))
+                curr_str = ""
+                curr_num = 0
+
             elif char == ']':
-                # Pop the context: string before the bracket and the repeat count
-                prev_string, repeat_count = stack.pop()
-                # Decode the current segment and append to the previous string
-                current_string = prev_string + (current_string * repeat_count)
+                prev_str, num = stack.pop()
+                curr_str = prev_str + num * curr_str
+
             else:
-                # Append letters to the current string
-                current_string += char
-                
-        return current_string
+                curr_str += char
+
+        return curr_str
         
