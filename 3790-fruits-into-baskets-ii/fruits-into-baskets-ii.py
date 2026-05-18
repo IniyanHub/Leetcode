12e42653da26ea0a079c0245793ad15439abd2dd
Iniyan <iniyan.workhub@gmail.com>
2026-05-18 23:00:44 +0530

    Time: 24 ms (34.54%) | Memory: 19.4 MB (7.46%) - LeetSync

---
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        n = len(fruits)
+        used = [False] * n
+        unplaced = 0
+
+        for fruit in fruits:
+            placed = False
+
+            # Find the leftmost available basket
+            for i in range(n):
+                if not used[i] and baskets[i] >= fruit:
+                    used[i] = True
+                    placed = True
+                    break
+
+            if not placed:
+                unplaced += 1
+
+        return unplaced
+        
