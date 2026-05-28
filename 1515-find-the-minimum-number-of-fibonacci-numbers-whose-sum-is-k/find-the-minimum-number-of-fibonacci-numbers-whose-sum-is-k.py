0f25e7564d8fe28964589d4457186b9968f278b1
Iniyan <iniyan.workhub@gmail.com>
2026-05-28 10:14:53 +0530

    Time: 2 ms (53.85%) | Memory: 19.4 MB (22.49%) - LeetSync

---
+++ 1515-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMinFibonacciNumbers(self, k: int) -> int:
+        fib = [1, 1]
+
+        while fib[-1] < k:
+            fib.append(fib[-1] + fib[-2])
+
+        count = 0
+        i = len(fib) - 1
+
+        # Greedy selection
+        while k > 0:
+
+            if fib[i] <= k:
+                k -= fib[i]
+                count += 1
+            else:
+                i -= 1
+
+        return count
+        
