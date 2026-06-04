0b33d7ac6172bb3017fd1e2f045c1ad5634e9c41
Iniyan <iniyan.workhub@gmail.com>
2026-06-04 11:09:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (21.78%) - LeetSync

---
+++ 1858-latest-time-by-replacing-hidden-digits/latest-time-by-replacing-hidden-digits.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maximumTime(self, time: str) -> str:
+        time = list(time)
+
+        # Hour first digit
+        if time[0] == '?':
+            if time[1] == '?' or time[1] <= '3':
+                time[0] = '2'
+            else:
+                time[0] = '1'
+
+        # Hour second digit
+        if time[1] == '?':
+            if time[0] == '2':
+                time[1] = '3'
+            else:
+                time[1] = '9'
+
+        # Minute tens digit
+        if time[3] == '?':
+            time[3] = '5'
+
+        # Minute ones digit
+        if time[4] == '?':
+            time[4] = '9'
+
+        return "".join(time)
+        
