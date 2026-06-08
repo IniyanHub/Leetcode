8fd843a30ae639b995d8a5d4c1dbf7b1f17cb0d2
Iniyan <iniyan.workhub@gmail.com>
2026-06-08 11:25:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (55.38%) - LeetSync

---
+++ 1139-previous-permutation-with-one-swap/previous-permutation-with-one-swap.py
@@ -0,0 +1,24 @@
+class Solution:
+    def prevPermOpt1(self, arr: List[int]) -> List[int]:
+        n = len(arr)
+
+        # Find first position to decrease
+        i = n - 2
+        while i >= 0 and arr[i] <= arr[i + 1]:
+            i -= 1
+
+        if i < 0:
+            return arr
+
+        # Find largest value smaller than arr[i]
+        j = n - 1
+        while arr[j] >= arr[i]:
+            j -= 1
+
+        # Move left through duplicates
+        while j > 0 and arr[j] == arr[j - 1]:
+            j -= 1
+
+        arr[i], arr[j] = arr[j], arr[i]
+        return arr
+        
