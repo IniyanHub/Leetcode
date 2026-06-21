c726f2ee0b23f777e421e9d7e89050fa28271e45
Iniyan <iniyan.workhub@gmail.com>
2026-06-21 19:05:07 +0530

    Time: 137 ms (57.10%) | Memory: 23.7 MB (76.03%) - LeetSync

---
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
@@ -0,0 +1,14 @@
+class Solution:
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        m, n = len(rowSum), len(colSum)
+        ans = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                val = min(rowSum[i], colSum[j])
+                ans[i][j] = val
+                rowSum[i] -= val
+                colSum[j] -= val
+
+        return ans
+        
